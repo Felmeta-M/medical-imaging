060ea74d954be4da14d8905836d2643a93aabb9e
Felmeta Muktar <felmetamuktar23@gmail.com>
2024-06-27 18:02:28 +0300

    app.py updating

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,32 @@
-from typing import Dict
+# from typing import Dict
 
-from fastapi import FastAPI, File, UploadFile
+# from fastapi import FastAPI, File, UploadFile
+# from fastapi.middleware.cors import CORSMiddleware
+# from src.ml_model.predict import get_result
+
+# # Define application
+# app = FastAPI(
+#     title="Medical Imaging app",
+#     description="Medical Imaging app!",
+#     version="0.1",
+# )
+
+# origins = ["*"]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# @app.post("/api/v1/predict")
+# async def predict(file: UploadFile = File(...)):
+#     result = get_result(file, is_api=True)
+#     return result
+
+from typing import Dict
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from src.ml_model.predict import get_result
 
@@ -22,5 +48,8 @@ app.add_middleware(
 
 @app.post("/api/v1/predict")
 async def predict(file: UploadFile = File(...)):
+    if file.content_type not in ["image/png"]:
+        raise HTTPException(status_code=400, detail="Please upload a chest X-ray image.")
+    
     result = get_result(file, is_api=True)
     return result
